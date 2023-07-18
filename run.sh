be0a6c8c10b7ce07eed419b8b352e89a8b159f22
Tao Zhuo <45383174+visiontao@users.noreply.github.com>
2023-07-18 16:25:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ python main.py --model pfcl --dataset cifar100 --n_classes 100 --n_tasks 20 --lr
 
 python main.py --model pfcl --dataset tinyimg --n_classes 200 --n_tasks 10 --lr 0.03 --n_epochs 100 --aux_dataset caltech256 --alpha 1.0 --device_id 0
 
-python main.py --model pfcl --dataset rot-mnist --n_classes 10 --n_tasks 20 --lr 0.03 --n_epochs 1 --aux_dataset caltech256 --alpha 1.0 --batch_size 64 --device_id 1 
+python main.py --model pfcl --dataset rot-mnist --n_classes 10 --n_tasks 20 --lr 0.03 --n_epochs 1 --aux_dataset caltech256 --alpha 1.0 --batch_size 64 --device_id 0
